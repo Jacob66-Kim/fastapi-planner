f5c92d56ce88f7cc7cfd3286c44d408005f3c6aa
Jacob66-Kim <jacob913.kim@gmail.com>
2023-06-28 22:42:48 +0900

    added models, routes

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
+from routes.users import user_router
+from routes.events import event_router
+import uvicorn
 
 app = FastAPI()
 
+app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/event")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
